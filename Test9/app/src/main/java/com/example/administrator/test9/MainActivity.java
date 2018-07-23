6e631775e228ba0a21516e16996ebd60f553febc
wxm <1174634507@qq.com>
2018-07-23 15:21:51 +0800

    nine-one

---
+++ Test9/app/src/main/java/com/example/administrator/test9/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.administrator.test9;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView=findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://www.baidu.com");
+    }
+}
