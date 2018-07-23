07f87a41976af42b652a147268316c7cd4c6eb04
wxm <1174634507@qq.com>
2018-07-23 15:42:38 +0800

    nine-two-one

--- Test9/app/src/main/java/com/example/administrator/test9/MainActivity.java
+++ Test9/app/src/main/java/com/example/administrator/test9/MainActivity.java
@@ -4,16 +4,53 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
-    WebView webView;
+    TextView textView;
+    private URL url;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        webView=findViewById(R.id.webView);
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.setWebViewClient(new WebViewClient());
-        webView.loadUrl("http://www.baidu.com");
+        textView=findViewById(R.id.textView);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    url = new URL("https://www.baidu.com");
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+
+                }
+                try {
+                    HttpURLConnection httpURLConnection= (HttpURLConnection) url.openConnection();
+                    httpURLConnection.setReadTimeout(5000);
+                    httpURLConnection.setRequestMethod("GET");
+                    httpURLConnection.setConnectTimeout(5000);
+                    InputStream inputStream=httpURLConnection.getInputStream();
+                    BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(inputStream));
+                    StringBuilder builder=new StringBuilder();
+                    String s=null;
+                    while ((s=bufferedReader.readLine())!=null){
+                        builder.append(s);
+                    }
+                    textView.setText(builder.toString());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
     }
 }
